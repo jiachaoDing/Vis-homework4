446964c994d9a889bca3a93d4098c967c28893d1
丁家超 <15103320+ding-jiachao@user.noreply.gitee.com>
2025-05-28 18:01:12 +0800

    再次尝试

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/homework4/',
+  base: process.env.NODE_ENV === 'production' ? '/Vis-homework4/' : '/',
   server: {
     host: '0.0.0.0',
     port: 3000,
     open: true
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
-  }
+
 }) 
