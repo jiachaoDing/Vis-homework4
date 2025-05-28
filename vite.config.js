550cadaa457e95832ee1b542409cd795d3542b5b
丁家超 <15103320+ding-jiachao@user.noreply.gitee.com>
2025-05-28 17:56:44 +0800

    尝试部署

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? '/homework4/' : '/',
+  base: '/homework4/',
   server: {
     host: '0.0.0.0',
     port: 3000,
